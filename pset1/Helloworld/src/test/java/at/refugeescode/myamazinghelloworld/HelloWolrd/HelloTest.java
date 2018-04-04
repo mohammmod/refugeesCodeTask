92d95c7f869e22688111a308cb99237206537d66
mohammmod <mohammad.sawas2@outlook.com>
2018-04-04 17:23:01 +0200

    this is my hello world

---
+++ pset1/Helloworld/src/test/java/at/refugeescode/myamazinghelloworld/HelloWolrd/HelloTest.java
@@ -0,0 +1,22 @@
+package at.refugeescode.myamazinghelloworld.HelloWolrd;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class HelloTest {
+    @Autowired
+    private Hello hello;
+    @Test
+    void getHello() {
+        Assert.assertThat(hello.getHello(),is("hello"));
+    }
+}
